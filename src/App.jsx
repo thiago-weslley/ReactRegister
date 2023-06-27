5ec773c4001a08061c75373823fcf3d7eb0977d0
Thiago Weslley <thiagoweslley@yahoo.com>
2023-06-27 15:26:08 -0300

    feat: Adicionar funcionalidade de cadastro de usuários usando Axios
    Adiciona a biblioteca Axios para permitir a comunicação entre o frontend e o backend. Implementa a função 'addNewUser' que realiza uma requisição POST para o endpoint 'http://localhost:3000/users' com os dados do novo usuário. Em seguida, atualiza o estado dos usuários com os dados do novo usuário cadastrado.

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,79 @@
-import React, { useState, useRef } from 'react';
-import Background from './assets/logoHome.svg';
-import Arrow from './assets/arrow.svg';
-import Trash from './assets/trash.svg'
-import { Container, Img, ContainerItems, H1, ImputWraper, Label, Input, Button, Ul, User, SpanUser, ButtonTrash } from './style';
+import React, { useState, useRef } from "react";
+import Axios from "axios";
 
+//IMG
+import Background from "./assets/logoHome.svg";
+import Arrow from "./assets/arrow.svg";
+import Trash from "./assets/trash.svg";
+
+import {
+  Container,
+  Img,
+  ContainerItems,
+  H1,
+  ImputWraper,
+  Label,
+  Input,
+  Button,
+  Ul,
+  User,
+  SpanUser,
+  ButtonTrash,
+} from "./style";
+import axios from "axios";
 
 const App = () => {
-  const hello = 'Olá!';
+  const hello = "Olá!";
   const [users, setUsers] = useState([]);
   const inputName = useRef();
   const inputAge = useRef();
 
+  const addNewUser = async () => {
+    const { data: newUser } = await axios.post("http://localhost:3000/users", {
+      name: inputName.current.value,
+      age: inputAge.current.value,
+    });
 
-
-  const addNewUser = () => {
-    setUsers([...users, { id: Math.random(), name: inputName.current.value, age: inputAge.current.value }]);
-  }
+    setUsers([...users, newUser]);
+  };
 
   const deleteUser = (userId) => {
-    const newUsers = users.filter(user => user.id !== userId);
-
-    setUsers(newUsers)
-  }
+    const newUsers = users.filter((user) => user.id !== userId);
+    setUsers(newUsers);
+  };
 
   return (
     <Container>
-      <Img src={Background} alt='Logo-imagem' />
+      <Img src={Background} alt="Logo-imagem" />
 
       <ContainerItems>
         <H1>{hello}</H1>
 
         <ImputWraper>
-          <Label htmlFor='name'>Nome:</Label>
-          <Input ref={inputName} id='name' type='text' placeholder='Nome' required />
+          <Label htmlFor="name">Nome:</Label>
+          <Input ref={inputName} id="name" type="text" placeholder="Nome" />
         </ImputWraper>
 
         <ImputWraper>
-          <Label htmlFor='age'>Idade:</Label>
-          <Input ref={inputAge} id='age' type='number' placeholder='Idade' required />
+          <Label htmlFor="age">Idade:</Label>
+          <Input ref={inputAge} id="age" type="number" placeholder="Idade" />
         </ImputWraper>
 
         <Button onClick={addNewUser}>
-          cadastrar<img src={Arrow} alt='arrow' />
+          cadastrar
+          <img src={Arrow} alt="arrow" />
         </Button>
 
         <Ul>
-          {
-            users.map(user => (
-              <User key={user.id}>
-                <SpanUser>{user.name}</SpanUser><span>{user.age}</span>
-                <ButtonTrash onClick={() => deleteUser(user.id)}><img src={Trash} alt='trash' /></ButtonTrash>
-              </User>
-            ))
-          }
+          {users.map((user) => (
+            <User key={user.id}>
+              <SpanUser>{user.name}</SpanUser>
+              <span>{user.age}</span>
+              <ButtonTrash onClick={() => deleteUser(user.id)}>
+                <img src={Trash} alt="trash" />
+              </ButtonTrash>
+            </User>
+          ))}
         </Ul>
       </ContainerItems>
     </Container>
