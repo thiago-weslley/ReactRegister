9ae353f61690d426d849c4e1987befabcf8aa0af
Thiago Weslley <thiagoweslley@yahoo.com>
2023-07-06 16:24:54 -0300

    refactor: Melhora a estrutura

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import GlobalStyle from "./styles/globalStyle.js";
 import Routes from "./routes.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.render(
   <React.StrictMode>
     <Routes />
     <GlobalStyle />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
